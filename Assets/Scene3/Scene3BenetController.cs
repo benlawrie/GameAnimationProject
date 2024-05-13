c16cd679dccb24923e05915453c1d9b3c909b7cb
Abhaykmr111 <AbhayKumarDeonath.Pandit@student.uts.edu.au>
2024-05-13 16:28:18 +1000

    changes

--- Assets/Scene3/Scene3BenetController.cs
+++ Assets/Scene3/Scene3BenetController.cs
@@ -4,57 +4,58 @@ using UnityEngine;
 
 public class BenetController2 : MonoBehaviour
 {
-    Animator anim;
+    private Animator anim;
     public GameObject torch;
     public GameObject hand;
     public GameObject target;
-    public GameObject Benet;
-    public GameObject trigger;
+
     public float IK_weight = 1.0f;
 
     //Walking
-    float translationSpeed = 0.005f;
-    float rotationSpeed = 0.5f;
+    public float translationSpeed = 3.0f; // Increased speed for better visibility
+    public float rotationSpeed = 100.0f; // Increased rotation speed
 
     void Start()
     {
-        anim = this.GetComponent<Animator>();
-        torch.transform.parent = hand.transform;
-        
+        anim = GetComponent<Animator>();
+        if (hand != null && torch != null)
+        {
+            torch.transform.SetParent(hand.transform);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * translationSpeed;
-        //translation += Time.deltaTime;
-        if(Input.GetKeyDown(KeyCode.Space)){
-            anim.SetBool("test", true);
-        }
-        transform.Translate(0, 0, translation);
-
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        rotation += Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
-
-        if(translation != 0){
-            anim.SetBool("isWalking", true);
-            if (translation < 0){
-                anim.SetFloat("charSpeed", -1.0f);
-            }
-            else{
-                anim.SetFloat("charSpeed", 1.0f);
-            }
-        }
-        else{
-            anim.SetBool("isWalking", false);
-        }
+        HandleMovement();
+        HandleRotation();
+        HandleAnimations();
     }
 
-    private void OnAnimatorIK(int layerIndex){
-        anim.SetIKPosition(AvatarIKGoal.RightHand, target.transform.position); 
-        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, IK_weight); 
-        
-    } 
+    void HandleMovement()
+    {
+        float translation = Input.GetAxis("Vertical") * translationSpeed * Time.deltaTime;
+        transform.Translate(0, 0, translation);
+    }
 
+    void HandleRotation()
+    {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+    }
+
+    void HandleAnimations()
+    {
+        bool isWalking = Mathf.Abs(Input.GetAxis("Vertical")) > 0;
+        anim.SetBool("isWalking", isWalking);
+        anim.SetFloat("charSpeed", Input.GetAxis("Vertical"));
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (target != null)
+        {
+            anim.SetIKPosition(AvatarIKGoal.RightHand, target.transform.position); 
+            anim.SetIKPositionWeight(AvatarIKGoal.RightHand, IK_weight); 
+        }
+    }
 }
